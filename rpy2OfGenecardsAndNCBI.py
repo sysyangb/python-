2ad192e6da3c0424e0f9e8b65038b029200dbce5
renyunshu <41281150+sysyangb@users.noreply.github.com>
2021-01-16 15:27:28 +0800

    Update rpy2OfGenecardsAndNCBI.py

--- rpy2OfGenecardsAndNCBI.py
+++ rpy2OfGenecardsAndNCBI.py
@@ -50,21 +50,64 @@ def getSummary(geneId):
 
 _input = sys.argv[1]
 _num = int(sys.argv[2])
+marker = sys.argv[3]
 ensgList = []
 for line in open(_input,"r"):
-    if re.search("^ensgId", line):
+    if re.search("^ensgId", line) or re.search("genesymbol",line):
         continue
     ensgList.append(line.rstrip("\n").split('\t')[_num])
 
 out = open("genes.txt", "a")
-headers = ["species", "ensgId", "entezeId", "hgncId", "strand", "length", "position", "summary"]
-out.write('\t'.join(headers) + '\n')
-for key in ensgList:
-    temp = ["Homo_sapiens", key, "-", "-", "-", "-", "-", "-"]
-    out.write("\t".join(temp) + '\n')
-out.close()
+if marker == "E":
+    headers = ["species", "ensgId", "entezeId", "hgncId", "strand", "length", "position", "summary"]
+    out.write('\t'.join(headers) + '\n')
+    for key in ensgList:
+        temp = ["Homo_sapiens", key, "-", "-", "-", "-", "-", "-"]
+        out.write("\t".join(temp) + '\n')
+    out.close()
+if marker == "G":
+    headers = ["species", "genesymbol" ,"ensgId", "entezeId", "hgncId", "strand", "length", "position", "summary"]
+    out.write('\t'.join(headers) + '\n')
+    for key in ensgList:
+        temp = ["Homo_sapiens", key,"-", "-", "-", "-", "-", "-", "-"]
+        out.write("\t".join(temp) + '\n')
+    out.close()
 
-rscript = '''
+rscriptG = '''
+library(XML)
+library(RCurl)
+library(httr)
+library(xml2)
+
+allgene <- read.table("genes.txt",header = T,sep = "\t",stringsAsFactors = F)
+for(i in 1:nrow(allgene)){
+  if(allgene[i,3]=="-"){
+    tryCatch({
+      url<-paste0("https://www.genecards.org/cgi-bin/carddisp.pl?gene=",allgene[i,2],"&keywords=",allgene[i,1])
+      print(paste0(i," --- scrapy genecards information of ",allgene[i,2]))
+      geneinformation <- GET(url,content_type_xml())
+      html <- htmlParse(geneinformation,encoding = 'utf-8')
+      #ensg id
+      allgene[i,3] <- as.character(xpathSApply(doc=html,path = "//div[@class='gc-subsection']/div[@class='gc-subsection-inner-wrap']/ul/li/a/text()",fun = xmlValue)[[3]])
+      #entrze id 
+      allgene[i,4] <- as.character(xpathSApply(doc=html,path = "//div[@class='gc-subsection']/div[@class='gc-subsection-inner-wrap']/ul/li/a/text()",fun = xmlValue)[[2]])
+      #hgnc id 
+      allgene[i,5] <- as.character(xpathSApply(doc=html,path = "//div[@class='gc-subsection']/div[@class='gc-subsection-inner-wrap']/ul/li/a/text()",fun = xmlValue)[[1]])
+      #strand
+      allgene[i,6] <- as.character(xpathSApply(doc = html,path = "//div[@class='gc-subsection']/div[@class='gc-subsection-inner-wrap']/div[@class='row']/div/dl/dd/text()",fun=xmlValue)[[3]])
+      #length
+      allgene[i,7] <- as.character(xpathSApply(doc = html,path = "//div[@class='gc-subsection']/div[@class='gc-subsection-inner-wrap']/div[@class='row']/div/dl/dd/text()",fun=xmlValue)[[2]])
+      #position
+      allgene[i,8] <- as.character(xpathSApply(doc = html,path = "//div[@class='gc-subsection']/div[@class='gc-subsection-inner-wrap']/div[@class='row']/div/dl/dt/text()",fun=xmlValue)[[1]])
+      #summary
+      allgene[i,9] <- as.character(gsub("\r\n","",xpathSApply(doc = html,path = "//*[@id='summaries']/div[1]",fun = xmlValue)))
+    }, error = function(e){print(paste0("Error ",i," ",allgene[i,2]))}
+    ) 
+    Sys.sleep(runif(1,min=1,max=2))}
+}
+write.table(allgene[,c(-2,-9)],sep = "\t",file = "genecards.txt")
+'''
+rscriptE = '''
 library(XML)
 library(RCurl)
 library(httr)
@@ -96,9 +139,12 @@ for(i in 1:nrow(allgene)){
 }
 write.table(allgene[,c(1:7)],sep = "\t",file = "genecards.txt")
 '''
-###开始爬去genecards信息
-robjects.r(rscript)
 
+###开始爬去genecards信息
+if marker == "G":
+    robjects.r(rscriptG)
+elif marker == "E":
+    robjects.r(rscriptE)
 ###开始爬去NCBI信息
 ensgIDdict = OrderedDict()
 for line in open("genecards.txt","r"):
@@ -110,7 +156,7 @@ for line in open("genecards.txt","r"):
     ensgIDdict[ensgid]['geneid'] = info[3].split('"')[1]
     ensgIDdict[ensgid]['genecards'] = '\t'.join(info[3:])
 
-out = open(sys.argv[3],"a")
+out = open(sys.argv[4],"a")
 out.write("ensgId"+'\t'+"geneSymbol"+'\t'+"full name"+'\t'+"gene type"+'\t'+"summary"+'\t'+"entrzeId"+'\t'+"HGNCId"+'\t'+"strand"+'\t'+"length"+"\t"+"position"+'\n')
 n = 1
 for key in ensgIDdict:
